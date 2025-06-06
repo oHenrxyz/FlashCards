46c8427f6a001d1303dca3261ba320c8ba73d9a5
oHenrxyz <oliveira.henrique01@escola.pr.gov.br>
2025-06-06 10:40:36 -0300

    0706

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Matematica',
+    'Quais são as 4 operações basicas da Matematica',
+    'adição, subtração, multiplicação e divisão'
+)
+criaCartao(
+    'Geografia',
+    'Qual o idioma mais dificil do mundo?',
+    'é o Pirahã, falado por uma tribo no sul do Amazonas'
+)
+criaCartao(
+    'Historia',
+    'Qual Foi o ano que Abraham Lincoln foi assasinado',
+    'Foi em 1865'
+)
+criaCartao(
+    'Programação',
+    'O que é o JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é o CSS?',
+    'O CSS é uma linguagem de estilização'
+)
+criaCartao(
+    'Programação',
+    'O que é o python',
+    'linguagem de programação versátil e de alto nível'
+)
